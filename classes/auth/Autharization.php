5ec82740fde62d467037f690289ecdb8849fcff5
Дмитрий <Dimentr159@gmail.com>
2022-01-12 00:00:40 +0300

    Добавлен класс авторизации

--- classes/auth/Autharization.php
+++ classes/auth/Autharization.php
@@ -1,1 +1,60 @@
 <?php
+
+namespace classes\auth;
+
+
+use classes\exceptions\auth\AutharizeException;
+
+abstract class Autharization
+{
+
+    public $id;
+    public $firstName;
+    public $lastName;
+
+    /**
+     * @return string - id пользователя в соц. сети
+     */
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string - имя пользователя
+     */
+    public function getFirstName(): string
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @return string - фамилия пользователя
+     */
+    public function getLastName(): string
+    {
+        return $this->lastName;
+    }
+
+    protected function getUserInfo($code)
+    {
+        if(!$code)
+        {
+            throw new AutharizeException('Не передан код в запрос');
+        }
+
+        $token = $this->getToken($code);
+
+        if(!isset($token['access_token']))
+        {
+            throw new AutharizeException('Отсуцтвует токен');
+        }
+
+        return $this->getUserInformation($token);
+    }
+
+    public abstract function isAuthorized();
+    protected abstract function getToken($code);
+    protected abstract function getUserInformation($token);
+
+}
--- classes/auth/VK.php
+++ classes/auth/VK.php
@@ -1,1 +1,78 @@
 <?php
+
+namespace classes\auth;
+
+use classes\exceptions\auth\AutharizeException;
+use classes\log\Log;
+
+include 'config/vk/config.php';
+class VK extends Autharization
+{
+    const START_VK = array(
+        'client_id'=>ID_VK,
+        'redirect_url'=> URL_VK,
+        'response_type'=>'code'
+    );
+
+
+    /**
+     * Проверка существования пользователя в сети Вконтакте
+     * @return bool - true, если пользователь зарегистрирован, false - если нет
+     */
+
+    public function isAuthorized(): bool
+    {
+        try
+        {
+               $userInfo = $this->getUserInfo($_GET['code']);
+
+               if(!isset($userInfo['id']))
+               {
+                   throw new AutharizeException('не найдет ID пользователя');
+               }
+
+              $this->id = $userInfo['id'];
+              $this->firstName = $userInfo['first_name'];
+              $this->lastName = $userInfo['last_name'];
+
+              return true;
+
+        }catch (AutharizeException $ex)
+        {
+            Log::writeLog($ex->getMessage());
+            return false;
+        }
+    }
+
+    protected function getToken($code)
+    {
+        $params = array(
+            'client_id'   => ID_VK,
+            'secret_key'  => SECRET_VK,
+            'redirect_url'=> URL_VK,
+            'code'        => $code
+        );
+
+        return json_decode(file_get_contents(URL_TOKEN_VK . '?' . urldecode(http_build_query($params))),true);
+
+    }
+
+    protected function getUserInformation($token)
+    {
+        $params = array(
+          'client_id'=> ID_VK,
+          'uids'=> 'uid, first_name, last_name',
+          'access_token' => $token['access_token'],
+          'v'=> VERSION_VK,
+        );
+
+        $userInfo = json_decode(file_get_contents(URL_ABOUT_USER . '?' . urldecode(http_build_query($params))),true);
+
+        if(isset($userInfo['response'][0]['id']))
+        {
+            $userInfo = $userInfo['response'][0];
+        }
+
+        return$userInfo;
+    }
+}
