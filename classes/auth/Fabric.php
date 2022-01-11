f3f1f9128d7892bf51a215276e33b71c7c35c509
Дмитрий <Dimentr159@gmail.com>
2022-01-12 00:10:34 +0300

    Добавлен класс Фабрика

--- classes/auth/Fabric.php
+++ classes/auth/Fabric.php
@@ -1,3 +1,25 @@
 <?php
 
 namespace classes\auth;
+
+class Fabric
+{
+    /**
+     * @param $provider
+     * @return VK|null - Экземпляр класса Authorization
+     */
+    public static function getAuthClass($provider): ?Authorization
+    {
+        switch ($provider)
+        {
+            case 'vk':
+                $result = new VK();
+                break;
+            default:
+                $result = null;
+                break;
+        }
+
+        return $result;
+    }
+}
